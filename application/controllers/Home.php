90fa25e7b6cb5600ec93cf8738116a0cb74274e6
Kevin Semuel Yuri Mulyadi <thesym14@gmail.com>
2022-07-30 00:06:16 +0700

    first commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('homepage');
+	}
+	
+	public function detailproduct()
+	{
+		$this->load->view('detailbarang');
+	}
+}
