abc7561856d77d10d8412c2f01a41da5e5d5cc6e
Kevin Semuel Yuri Mulyadi <thesym14@gmail.com>
2022-09-28 22:54:44 +0700

    change headervid

--- application/views/content/homepage.php
+++ application/views/content/homepage.php
@@ -2,7 +2,7 @@
     <header>
         <div class="overlay"></div>
         <video preload="" playsinline="" autoplay="" muted="" loop="" __idm_id__="2670593">
-            <source src="<?= base_url() ?>assets/headervid.m4v" type="video/mp4">
+            <source src="assets/headervid.m4v" type="video/mp4">
         </video>
      
 
