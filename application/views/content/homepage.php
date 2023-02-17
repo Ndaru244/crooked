056cd94e16c23fcbed1241311b92307dd8ac5df2
Ndaru Langgeng Santosa <kring08569@gmail.com>
2023-02-18 02:36:01 +0700

    change: homepage render video sesuai device

--- application/views/content/homepage.php
+++ application/views/content/homepage.php
@@ -1,12 +1,19 @@
     <!-- Header-->
     <header>
         <div class="overlay"></div>
-        <video class="video-desktop" preload="" playsinline="" autoplay="" muted="" loop="">
-            <source src="assets/headervid.m4v" type="video/mp4">
-        </video>
-        <video class="video-mobile" preload="" playsinline="" autoplay="" muted="" loop="">
-            <source src="assets/headermobile.mp4" type="video/mp4">
-        </video>
+        <?php
+        $useragent = $_SERVER['HTTP_USER_AGENT'];
+
+        if (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i', $useragent) || preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i', substr($useragent, 0, 4))) {
+        ?>
+            <video class="video-mobile" preload="" playsinline="" autoplay="" muted="" loop="">
+                <source src="assets/headermobile.mp4" type="video/mp4">
+            </video>
+        <?php } else { ?>
+            <video class="video-desktop" preload="" playsinline="" autoplay="" muted="" loop="">
+                <source src="assets/headervid.m4v" type="video/mp4">
+            </video>
+        <?php } ?>
 
         <div class="container h-100">
             <div class="d-flex h-100 text-center align-items-center">
@@ -21,45 +28,45 @@
     <!-- Section-->
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
-            
+
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                <?php foreach ($datas as $get) :?>
-                <div class="col mb-5">
-                    <div class="card h-100">
-                        <!-- Sale badge-->
-                        <?= $get->price_sale == 0 ? "" : "<div class='badge bg-danger text-white position-absolute' style='top: 0.5rem; right: 0.5rem'><b>On Sale</b></div>" ?>
-                        <!-- Product image-->
-                        <img class="card-img-top" src="<?= base_url('assets/product_images/'.$get->product_picture)?>" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder"><?= $get->product_name ?></h5>
-                                <!-- Product price-->
-                                <?php
-                                if ($get->price_sale == 0) {
-                                    echo 'Rp.'.number_format($get->product_price, 0 ,',','.');
-                                } elseif ($get->price_sale != 0) {
-                                    echo "
-                                    <span class='text-muted text-decoration-line-through'><i>Rp.".number_format($get->product_price, 0, ',', '.')."</i></span>
-                                     <br/> Rp.".number_format($get->price_sale, 0, ',', '.')."";
-                                }
-                                ?>
+                <?php foreach ($datas as $get) : ?>
+                    <div class="col mb-5">
+                        <div class="card h-100">
+                            <!-- Sale badge-->
+                            <?= $get->price_sale == 0 ? "" : "<div class='badge bg-danger text-white position-absolute' style='top: 0.5rem; right: 0.5rem'><b>On Sale</b></div>" ?>
+                            <!-- Product image-->
+                            <img class="card-img-top" src="<?= base_url('assets/product_images/' . $get->product_picture) ?>" alt="..." />
+                            <!-- Product details-->
+                            <div class="card-body p-4">
+                                <div class="text-center">
+                                    <!-- Product name-->
+                                    <h5 class="fw-bolder"><?= $get->product_name ?></h5>
+                                    <!-- Product price-->
+                                    <?php
+                                    if ($get->price_sale == 0) {
+                                        echo 'Rp.' . number_format($get->product_price, 0, ',', '.');
+                                    } elseif ($get->price_sale != 0) {
+                                        echo "
+                                    <span class='text-muted text-decoration-line-through'><i>Rp." . number_format($get->product_price, 0, ',', '.') . "</i></span>
+                                     <br/> Rp." . number_format($get->price_sale, 0, ',', '.') . "";
+                                    }
+                                    ?>
+                                </div>
+                            </div>
+                            <!-- Product actions-->
+                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="<?= base_url('detail/' . $get->id_product) ?>">Detail Products</a></div>
                             </div>
                         </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="<?= base_url('detail/'.$get->id_product) ?>">Detail Products</a></div>
-                        </div>
                     </div>
-                </div>
                 <?php endforeach; ?>
             </div>
 
         </div>
 
         <div class="text-center">
-            <a class="btn btn-outline-dark mt-auto" href="<?= base_url('main/shopping') ?>" >Selengkapnya</a>
-            
+            <a class="btn btn-outline-dark mt-auto" href="<?= base_url('main/shopping') ?>">Selengkapnya</a>
+
         </div>
     </section>
